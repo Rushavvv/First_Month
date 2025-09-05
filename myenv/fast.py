a475f045268a36bc8ab6f5c49bf49d79718ac8e2
Rushav Sthapit <rushav@MacBook-Pro-3.local>
2025-09-05 14:58:16 +0545

    :wrench: More on FastAPI

--- myenv/fast.py
+++ myenv/fast.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -22,3 +22,30 @@ async def about():
 async def view():
     data = load_data()
     return data
+
+@app.get('/patient/{patient_id}')
+async def view_patient(patient_id: str = Path(..., description = 'Id of the patient', example = 'P009')):
+    #loading all the patients 
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code = 404, detail = 'Patient not found') 
+     
+@app.get('/sort')
+async def sort_patients(sort_by: str = Query(..., description = 'The way you want the data to be sorted', 
+example = 'By height or weight'), order: str = Query('asc', description = 'Sort in ascending order')):
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code = 400, detail = f'Invalid field selected from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code = 400, detail = f'Invalid, select either asc or desc')
+
+    data = load_data()
+
+    sort_order = True if order == 'desc' else False
+
+    sorted_data = sorted(data.values(), keys = lambda x: x.get(sort_by, 0), reverse = sort_order)
+
+    return sorted_data
